5cae205e87258457c4ed62282d0dbb54ef5e05e8
Jinwei <renjwjx@gmail.com>
2021-01-02 22:18:25 +0800

    nscache demo

--- NSCacheDemo/ViewController.m
+++ NSCacheDemo/ViewController.m
@@ -9,15 +9,48 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) NSCache* cache;
 @end
 
 @implementation ViewController
+
+- (NSCache *)cache
+{
+    if (!_cache) {
+        _cache = [[NSCache alloc] init];
+        _cache.totalCostLimit = 20;
+        _cache.countLimit = 8;
+    }
+    return _cache;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)addData:(id)sender {
+    NSURL* url = [NSURL URLWithString:@"https://tse4-mm.cn.bing.net/th/id/OIP.XZIluAO4s8Q4M3DQQs5lSAHaLL?pid=Api&rs=1"];
+    for (int i = 0 ; i < 10; i++) {
+        NSData* data = [NSData dataWithContentsOfURL:url];
+        [self.cache setObject:data forKey:@(i) cost:2];
+        NSLog(@"add data %d", i);
+    }
+    
+}
+- (IBAction)checkData:(id)sender {
+    for (int i = 0; i < 10; i ++) {
+        NSData* data = [self.cache objectForKey:@(i)];
+        if (data) {
+            NSLog(@"get data, %d", i);
+        }
+    }
+    
+}
+- (IBAction)clearData:(id)sender {
+    NSLog(@"clear ...");
+    [self.cache removeAllObjects];
+    
+}
 
 @end
